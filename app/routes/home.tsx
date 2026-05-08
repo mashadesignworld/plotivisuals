112c12caa6aa13a7304ee0d29296bae9fe90eb81
YourNewGitHubUsername <njugunak.kevin@gmail.com>
2026-05-09 01:11:50 +0300

    Second

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
+import { ArrowRight } from "lucide-react";
 import Navbar from "../../components/Navbar";
 import type { Route } from "./+types/home";
+import { Button } from "../../components/ui/Button";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -13,7 +15,22 @@ export default function Home() {
   return (
   <div className="home">
     <Navbar />
-    <h1 className="text-3xl text-indigo-700 
-  font-bold">Home</h1></div>
+    <section className="hero">
+      <div className="announce">
+        <div className="dot">
+          <div className="pulse">
+
+          </div>
+        </div>
+        <p>Introducing PlotiVisuals AI 2.0</p>
+      </div>
+      <h1>Turn Your Plot into a Palace with AI-Powered 3D Renders.</h1>
+      <p className="subtitle">Stop guessing what your house will look like. Upload your 2D blue-prints and watch our AI generate photorealistic 3D renders in seconds—optimized for the Kenyan landscape.</p>
+      <div className="actions">
+        <a href="#upload" className="cta">Start Building <ArrowRight className="icon"/></a>
+        <Button variant="outline" size="lg" className="demo">Learn More</Button>
+      </div>
+      </section>
+      </div>
 );
 }
