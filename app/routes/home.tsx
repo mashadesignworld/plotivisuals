a2417ed589fb4252e081c2c236d7f05d5cef5a39
YourNewGitHubUsername <njugunak.kevin@gmail.com>
2026-05-10 23:06:53 +0300

    project manhattan

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Layers , Clock} from "lucide-react";
 import Navbar from "../../components/Navbar";
 import type { Route } from "./+types/home";
 import { Button } from "../../components/ui/Button";
@@ -30,6 +30,55 @@ export default function Home() {
         <a href="#upload" className="cta">Start Building <ArrowRight className="icon"/></a>
         <Button variant="outline" size="lg" className="demo">Learn More</Button>
       </div>
+      <div id="upload" className="upload-shell">
+        <div className="grid-overlay"/>
+        <div className="upload-card">
+          <div className="upload-head">
+            <div className="upload-icon">
+                <Layers className="icon"/>
+            </div>
+            <h3>
+              Upload your  floor plan and let our AI do the rest.
+            </h3>
+            <p>Supports JPG, PNG, formats up to 10MB</p>
+          </div>
+          <p>Upload Images</p>
+        </div>
+
+      </div>
+      </section>
+
+      <section className="projects">
+          <div className="section-inner">
+            <div className="section-head">
+                <div className="copy">
+                    <h2>Projects</h2>
+                    <p>Your Latest work and shared
+                      community projects, all in one place</p>
+                </div>
+            </div>
+            <div className="projects-grid">
+              <div className="project-card group">
+                <div className="preview"/>
+                  <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                  alt="Project"/>
+                  <div className="badge">
+                    <span>
+                      Community
+                    </span>
+                  </div>
+                </div>
+                <div className="card-body">
+                    <div>
+                      <h3>Project Manhattan</h3>
+                      <div>
+                        <Clock size={12} />
+                        <span>{new Date('01.01.2027').toLocaleDateString()}</span>
+                      </div>
+                    </div>
+                </div>
+                </div>
+          </div>
       </section>
       </div>
 );
